5e4f949c35acdbac7ec47e265ce599ca535cbb66
darksuei <chrisraphael576@gmail.com>
2024-11-07 09:40:48 +0100

    chore: cleanup charts & networking stuff

--- client-svc/index.js
+++ client-svc/index.js
@@ -1,9 +1,14 @@
 import express from "express";
-import {KubeRPCClient, KubeRPCServer} from "kuberpc-sdk";
-const app = express();
+import { KubeRPCClient, KubeRPCServer } from "kuberpc-sdk";
+import dotenv from "dotenv";
 
-const TCP_PORT = 8092;
-const HTTP_PORT = 8091;
+const app = express();
+dotenv.config();
+
+const RPC_PORT = process.env.RPC_PORT || 8092;
+const HTTP_PORT = process.env.HTTP_PORT || 8091;
+const KUBERPC_CORE = process.env.KUBERPC_CORE;
+const SERVICE_NAME = process.env.SERVICE_NAME;
 
 app.get("/", (req, res) => {
   return res.send("Hello World");
@@ -16,22 +21,22 @@ app.listen(HTTP_PORT, async () => {
 
 async function initRPCClient() {
   const kubeRPCClient = new KubeRPCClient({
-    apiBaseURL: "http://localhost:8080",
+    apiBaseURL: KUBERPC_CORE,
     timeout: 10000,
-    retries: 2
+    retries: 2,
   });
 
   await kubeRPCClient.initialize();
 
   console.log(await kubeRPCClient.getServices());
-  console.log(await kubeRPCClient.getSingleService('test-svc-01'));
+  console.log(await kubeRPCClient.getSingleService(SERVICE_NAME));
 
   const response = await kubeRPCClient.invokeMethod({
-    serviceName: "test-svc-01",
+    serviceName: SERVICE_NAME,
     method: {
       name: "test-method-01",
       params: ["param_one", "param_two"],
-    }
+    },
   });
-  console.log(response)
+  console.log(response);
 }
--- server-svc/index.js
+++ server-svc/index.js
@@ -1,9 +1,15 @@
 import express from "express";
-import {KubeRPCClient, KubeRPCServer} from "kuberpc-sdk";
+import { KubeRPCClient, KubeRPCServer } from "kuberpc-sdk";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 
-const TCP_PORT = 8082;
-const HTTP_PORT = 8081;
+const RPC_HOST = process.env.RPC_HOST || "localhost";
+const RPC_PORT = Number(process.env.RPC_PORT || 8082);
+const HTTP_PORT = Number(process.env.HTTP_PORT || 8081);
+const KUBERPC_CORE = process.env.KUBERPC_CORE;
+const SERVICE_NAME = process.env.SERVICE_NAME;
 
 app.get("/", (req, res) => {
   return res.send("Hello World");
@@ -16,23 +22,23 @@ app.listen(HTTP_PORT, async () => {
 
 async function initRPCServer() {
   const kubeRPCServer = new KubeRPCServer({
-    apiBaseURL: "http://localhost:8080",
-    port: TCP_PORT,
-    serviceName: "test-svc-01",
+    apiBaseURL: KUBERPC_CORE,
+    port: RPC_PORT,
+    serviceName: SERVICE_NAME,
   });
 
   await kubeRPCServer.initialize();
 
   await kubeRPCServer.registerSingleMethod({
-    host: "localhost",
-    port: TCP_PORT,
-    serviceName: "test-svc-01",
+    host: RPC_HOST,
+    port: RPC_PORT,
+    serviceName: SERVICE_NAME,
     method: {
       name: "test-method-01",
       params: ["param_one"],
       description: "Testing RPC",
       handler: testMethod,
-    }
+    },
   });
 }
 
